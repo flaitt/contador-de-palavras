4a79ad5f5934ea84b04bf97b3aa9687913a66d3a
flaitt <flaittj@gmail.com>
2017-11-24 10:58:18 -0200

    programa esta funcionando

--- src/main.c
+++ src/main.c
@@ -9,14 +9,51 @@
 
 int main() {
 
-  char c;
-
-  c = 1;
+  char c, cAnterior;
+  int contador=0, ncomeca=1, ehNumero=0, antesVirgula=0, depoisVirgula=0;
+  
+  c = ' ';
 
   while (c != '\n') {
+    cAnterior=c;
+    
     scanf("%c", &c);
+    
+    if(c=='0' || c=='1' || c=='2' || c=='3' || c=='4' || c=='5' || c=='6' || c=='7'|| c=='8'|| c=='9'){
+        antesVirgula=1;
+    }
+        
+    if((c=='0'|| c=='1' || c=='2' || c=='3' || c=='4' || c=='5' || c=='6' || c=='7'|| c=='8'|| c=='9') && (cAnterior=='.' || cAnterior==',')){
+        depoisVirgula=1;
+    }
+    
+    if(antesVirgula==1 && depoisVirgula==1){
+        ehNumero=1;
+    }
+    
+    if((c=='.' || c==',' || c=='!' || c=='-') && cAnterior == ' '){
+        ncomeca=0;
+    }
+    
+    if(c!='.' && c!=',' && c!='!' && c!= '-' && c!=' ' && c!='\n'){
+        ncomeca=1;
+    }
+    
+    if((((c == ' ' || c=='\n') && cAnterior != ' ') && ncomeca==1) || ((cAnterior=='.' || cAnterior==',' || cAnterior=='-' || cAnterior=='!') && (c!=' ' && c!='.' && c!=',' && c!='-' && c!='\n' && c!='!'))){
+        contador++;
+    }
+    
+        
+    if(ehNumero==1){
+        antesVirgula=0;
+        depoisVirgula=0;
+        ehNumero=0;
+        contador--;
+    }
+    
   }
+  
 
-  printf("1\n");
+  printf("%i\n",contador);
   return 0;
 }
